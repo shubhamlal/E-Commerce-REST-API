4d1986fe78e9064eb018d392a4ee868ea5fcdcc3
Shubham Lal <shubhamlal400@gmail.com>
2019-06-19 14:11:58 +0530

    Controller change

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 require('./server/Controllers/ProductController')
+require('./server/Controllers/OrderController')
--- server/Controllers/OrderController.js
+++ server/Controllers/OrderController.js
@@ -1,14 +1,19 @@
-const express = require('express')
+const PORT = 7879;
+const express = require("express");
 
 const Order = require('../models/Order')
 var app = express()
 
 app.get('/Orders/:id', (req,res) => {
-    
+    res.send(req.body)
 })
 
-app.post('/Orders')
+//app.post('/Orders')
 
-app.put()
+//app.put()
 
-app.delete()
+//app.delete()
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at `+ PORT);
+})
